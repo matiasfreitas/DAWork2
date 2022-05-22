95a8c287c24d280ed0f90d3416e8ca7aaf46f3c5
up202006111 <96194302+up202006111@users.noreply.github.com>
2022-05-22 22:11:44 +0100

    Add files via upload

---
+++ Source/Travels.cpp
@@ -0,0 +1,94 @@
+//
+// Created by frant on 22/05/2022.
+//
+
+#include "Travels.h"
+
+Travel::Travel(int origin, int destination, int capacity, int duration): origin(origin), destination(destination),capacity(capacity), duration(duration){}
+
+Travel::Travel() {}
+
+
+/**
+ * This override operator return true if volume, weight, payment and duration is equal.
+ * @param rhs other package to compare
+ */
+bool Travel::operator==(const Travel &rhs) const {
+    return origin == rhs.origin &&
+           destination == rhs.destination &&
+           capacity == rhs.capacity &&
+           duration == rhs.duration;
+}
+
+/**
+ * This override operator return true if volume, weight, payment or duration is unequal.
+ * @param rhs other package to compare
+ */
+bool Travel::operator!=(const Travel &rhs) const {
+    return !(rhs == *this);
+}
+
+
+/**
+ * This override operator > return true if that instance is more light and after volume than rhs
+ * @param rhs other package to compare
+ */
+bool Travel::operator<(const Travel &rhs) const {
+    if (destination < rhs.destination)
+        return true;
+    if (rhs.destination < destination)
+        return false;
+    return origin< rhs.origin;
+}
+
+/**
+ * This override operator < return tru if that instance os more heavy and after volume than rhs
+ * @param rhs other package to compare
+ */
+bool Travel::operator>(const Travel &rhs) const {
+    return rhs < *this;
+}
+
+/**
+ * This override operator <= return true if that instance is more light and after volume or equal than rhs
+ * @param rhs other package to compare
+ */
+bool Travel::operator<=(const Travel &rhs) const {
+    return !(rhs < *this);
+}
+
+/**
+ * This override operator >= return true if that instance is more heavy and after volume or equal than rhs
+ * @param rhs other package to compare
+ */
+bool Travel::operator>=(const Travel &rhs) const {
+    return !(*this < rhs);
+}
+/**
+ * This override operator extractor operator to out information about this object
+ * @param os where is going the output
+ */
+std::ostream &operator<<(std::ostream &os, const Travel &aTravel) {
+    os << "origin: " << aTravel.origin << " destination: " << aTravel.destination << " capacity: " << aTravel.capacity
+       << " duration: " << aTravel.duration;
+    return os;
+}
+
+
+
+int Travel::getorigin() const {
+    return origin;
+}
+
+int Travel::getdestination() const {
+    return destination;
+}
+
+int Travel::getcapacity() const {
+    return capacity;
+}
+
+int Travel::getduration() const {
+    return duration;
+}
+
